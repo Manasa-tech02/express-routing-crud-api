d4b05a50ad1f9b968b92622842400f5314158bc0
Manasa-tech02 <manasasheri02@gmail.com>
2026-02-13 15:40:09 +0530

    feat: Integrate Prisma ORM with PostgreSQL, define initial User schema, and configure the Prisma client.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@prisma/client'
+import "dotenv/config";
 
-const prisma = new PrismaClient();
 
-export default prisma;
+const connectionString = `${process.env.DATABASE_URL}`
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
+
+export default prisma
